319f946494a21f7b08609a9eedc4273a355fd8b8
Moeefa <moeefa@protonmail.com>
2024-09-14 01:23:33 -0400

    🐛 fix: add timeout to locale button

--- src/components/locale-toggle.tsx
+++ src/components/locale-toggle.tsx
@@ -20,6 +20,7 @@ import { useLocale, useTranslations } from "next-intl";
 import { GlobeIcon } from "@radix-ui/react-icons";
 import Image from "next/image";
 import { cn } from "@/lib/utils";
+import { useState } from "react";
 import { useTheme } from "next-themes";
 
 const DATA = [
@@ -49,15 +50,23 @@ const DATA = [
   },
 ];
 
+let closeTimeout: NodeJS.Timeout;
+
 export function LocaleToggle() {
   const pathname = usePathname();
   const router = useRouter();
   const locale = useLocale();
   const t = useTranslations();
 
+  const [open, setOpen] = useState(false);
+
   return (
     <Select
       value={DATA.find((item) => item.value === locale)?.value}
+      onOpenChange={(isOpen) => {
+        closeTimeout = setTimeout(() => setOpen(isOpen), 150);
+      }}
+      open={open}
       onValueChange={(value: "en-US" | "pt-BR") =>
         router.replace(pathname, { locale: value, scroll: false })
       }
